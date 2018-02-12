0eb4e614328b685085cc32513b8d1d7a0a77fba7
Mirmohammad Saadati <mir@MirmohammadsMBP.local>
2018-02-11 23:02:20 -0500

    Changed BlockReader, Removed Schema, Added Student

---
+++ src/com/mp1/MP1.java
@@ -0,0 +1,19 @@
+package com.mp1;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MP1 {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		BlockReader br = new BlockReader();
+		br.open("input.txt");
+		List<Student> list = br.nextBlock();
+		for (Student s : list)
+			System.out.println(s);
+	}
+}
