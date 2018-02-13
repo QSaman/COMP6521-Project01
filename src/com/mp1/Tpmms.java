59b28ad94f51e8196540c9573e5c7b25e6e8065d
Saman Saadi <QSaman@users.noreply.github.com>
2018-02-12 23:28:58 -0500

    Add Tpmms class

---
+++ src/com/mp1/Tpmms.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.mp1;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.mp1.disk.BlockReader;
+
+/**
+ * @author saman
+ *
+ */
+public class Tpmms {
+	
+	ArrayList<MemoryBuffer> buffers;
+	BlockReader inputReader;
+
+	/**
+	 * @throws FileNotFoundException 
+	 * 
+	 */
+	public Tpmms(String input_file_name) throws FileNotFoundException 
+	{
+		buffers = new ArrayList<>();
+		inputReader = new BlockReader();
+		inputReader.open(input_file_name);
+	}
+	
+	public boolean phase1() throws IOException
+	{
+		boolean two_phase = true;
+		int buffer_cnt = 0;
+		while (true)
+		{
+			if (Runtime.getRuntime().freeMemory() <= 400)
+			{
+				two_phase = false;
+				break;
+			}			
+			MemoryBuffer buffer = new MemoryBuffer();
+			inputReader.nextBlock(buffer);
+			if (buffer.size() == 0)
+				break;
+			++buffer_cnt;
+			buffers.add(buffer);
+		}
+		return two_phase;
+	}
+
+}
