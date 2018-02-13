d85ee5014a2efc9cbb51b91d44fd294fba38be4f
mirmohammad <mir.mohammad.saadati@gmail.com>
2018-02-13 16:17:17 -0500

    Added project to my intelliJ (I have eclipse :D)

--- src/com/mp1/Student.java
+++ src/com/mp1/Student.java
@@ -4,183 +4,171 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * @author mirmohammad
- *
  */
 public class Student implements Comparable<Student> {
 
-	//TODO If we don't use the following static variables, remove them for saving more memory
-	public static final int student_id_len = 8;
-	public static final int first_name_len = 10;
-	public static final int last_name_len = 10;
-	public static final int department_len = 3;
-	public static final int program_len = 3;
-	public static final int sin_number_len = 9;
-	public static final int address_len = 57;
+    private int studentId;
+    private byte[] firstName = new byte[10];
+    private byte[] lastName = new byte[10];
+    private short department;
+    private short program;
+    private int sinNumber;
+    private byte[] address = new byte[57];
 
-	private int studentId;
-	private byte[] firstName = new byte[10];
-	private byte[] lastName = new byte[10];
-	private short department;
-	private short program;
-	private int sinNumber;
-	private byte[] address = new byte[57];
+    public Student() {
+    }
 
-	public Student() {
-	}
-	
-	public void parseLine(String line)
-	{
-		studentId = Integer.parseInt(line.substring(0, 8));
-		firstName = line.substring(8, 18).getBytes(StandardCharsets.US_ASCII);
-		lastName = line.substring(18, 28).getBytes(StandardCharsets.US_ASCII);
-		department = Short.parseShort(line.substring(28, 31));
-		program = Short.parseShort(line.substring(31, 34));
-		sinNumber = Integer.parseInt(line.substring(34, 43));
-		address = line.substring(43, 100).getBytes(StandardCharsets.US_ASCII);
-	}
-	
-	public byte[] getBytes()
-	{
-		byte[] ret = new byte[100];
-		
-		System.arraycopy(String.format("%08d", studentId).getBytes(StandardCharsets.US_ASCII), 
-				0, ret, 0, 8);
-		System.arraycopy(firstName, 0, ret, 8, 10);
-		System.arraycopy(lastName, 0, ret, 18, 10);
-		System.arraycopy(String.format("%03d", department).getBytes(StandardCharsets.US_ASCII), 
-				0, ret, 28, 3);
-		System.arraycopy(String.format("%03d", program).getBytes(StandardCharsets.US_ASCII), 
-				0, ret, 31, 3);
-		System.arraycopy(String.format("%09d", sinNumber).getBytes(StandardCharsets.US_ASCII), 
-				0, ret, 34, 9);
-		System.arraycopy(address, 0, ret, 43, 57);
-		return ret;
-	}
+    public void parseLine(String line) {
+        studentId = Integer.parseInt(line.substring(0, 8));
+        firstName = line.substring(8, 18).getBytes(StandardCharsets.US_ASCII);
+        lastName = line.substring(18, 28).getBytes(StandardCharsets.US_ASCII);
+        department = Short.parseShort(line.substring(28, 31));
+        program = Short.parseShort(line.substring(31, 34));
+        sinNumber = Integer.parseInt(line.substring(34, 43));
+        address = line.substring(43, 100).getBytes(StandardCharsets.US_ASCII);
+    }
+
+    public byte[] getBytes() {
+        byte[] ret = new byte[100];
+
+        System.arraycopy(String.format("%08d", studentId).getBytes(StandardCharsets.US_ASCII),
+                0, ret, 0, 8);
+        System.arraycopy(firstName, 0, ret, 8, 10);
+        System.arraycopy(lastName, 0, ret, 18, 10);
+        System.arraycopy(String.format("%03d", department).getBytes(StandardCharsets.US_ASCII),
+                0, ret, 28, 3);
+        System.arraycopy(String.format("%03d", program).getBytes(StandardCharsets.US_ASCII),
+                0, ret, 31, 3);
+        System.arraycopy(String.format("%09d", sinNumber).getBytes(StandardCharsets.US_ASCII),
+                0, ret, 34, 9);
+        System.arraycopy(address, 0, ret, 43, 57);
+        return ret;
+    }
 
 
-	public byte[] getFirstName() {
-		return firstName;
-	}
+    public byte[] getFirstName() {
+        return firstName;
+    }
 
-	public void setFirstName(byte[] firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(byte[] firstName) {
+        this.firstName = firstName;
+    }
 
-	public byte[] getLastName() {
-		return lastName;
-	}
+    public byte[] getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(byte[] lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(byte[] lastName) {
+        this.lastName = lastName;
+    }
 
-	public byte[] getAddress() {
-		return address;
-	}
+    public byte[] getAddress() {
+        return address;
+    }
 
-	public void setAddress(byte[] address) {
-		this.address = address;
-	}		
+    public void setAddress(byte[] address) {
+        this.address = address;
+    }
 
-	/**
-	 * @return the studentId
-	 */
-	public int getStudentId() {
-		return studentId;
-	}
+    /**
+     * @return the studentId
+     */
+    public int getStudentId() {
+        return studentId;
+    }
 
-	/**
-	 * @param studentId the studentId to set
-	 */
-	public void setStudentId(int studentId) {
-		this.studentId = studentId;
-	}
+    /**
+     * @param studentId the studentId to set
+     */
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
 
-	/**
-	 * @return the department
-	 */
-	public short getDepartment() {
-		return department;
-	}
+    /**
+     * @return the department
+     */
+    public short getDepartment() {
+        return department;
+    }
 
-	/**
-	 * @param department the department to set
-	 */
-	public void setDepartment(short department) {
-		this.department = department;
-	}
+    /**
+     * @param department the department to set
+     */
+    public void setDepartment(short department) {
+        this.department = department;
+    }
 
-	/**
-	 * @return the program
-	 */
-	public short getProgram() {
-		return program;
-	}
+    /**
+     * @return the program
+     */
+    public short getProgram() {
+        return program;
+    }
 
-	/**
-	 * @param program the program to set
-	 */
-	public void setProgram(short program) {
-		this.program = program;
-	}
+    /**
+     * @param program the program to set
+     */
+    public void setProgram(short program) {
+        this.program = program;
+    }
 
-	/**
-	 * @return the sinNumber
-	 */
-	public int getSinNumber() {
-		return sinNumber;
-	}
+    /**
+     * @return the sinNumber
+     */
+    public int getSinNumber() {
+        return sinNumber;
+    }
 
-	/**
-	 * @param sinNumber the sinNumber to set
-	 */
-	public void setSinNumber(int sinNumber) {
-		this.sinNumber = sinNumber;
-	}
+    /**
+     * @param sinNumber the sinNumber to set
+     */
+    public void setSinNumber(int sinNumber) {
+        this.sinNumber = sinNumber;
+    }
 
-	@Override
-	public String toString() {
-		String[] fields = new String[] { Integer.toString(studentId), new String(firstName), new String(lastName),
-				Short.toString(department), Short.toString(program), Integer.toString(sinNumber), new String(address) };
-		return "Student ID: " + fields[0] + "\r\nFirst Name: " + fields[1] + "\r\nLast Name: " + fields[2]
-				+ "\r\nDepartment: " + fields[3] + "\r\nProgram: " + fields[4] + "\r\nSIN Number: " + fields[5]
-				+ "\r\nAddress: " + fields[6];
-	}
+    @Override
+    public String toString() {
+        String[] fields = new String[]{Integer.toString(studentId), new String(firstName), new String(lastName),
+                Short.toString(department), Short.toString(program), Integer.toString(sinNumber), new String(address)};
+        return "Student ID: " + fields[0] + "\r\nFirst Name: " + fields[1] + "\r\nLast Name: " + fields[2]
+                + "\r\nDepartment: " + fields[3] + "\r\nProgram: " + fields[4] + "\r\nSIN Number: " + fields[5]
+                + "\r\nAddress: " + fields[6];
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + studentId;
-		return result;
-	}
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + studentId;
+        return result;
+    }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof Student))
-			return false;
-		Student other = (Student) obj;
-		if (studentId != other.studentId)
-			return false;
-		return true;
-	}
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Student))
+            return false;
+        Student other = (Student) obj;
+        if (studentId != other.studentId)
+            return false;
+        return true;
+    }
 
-	@Override
-	public int compareTo(Student o) {
-		return studentId - o.getStudentId();
-	}
-	
-	public boolean isLessThan(Student o) {
-		return studentId - o.getStudentId() < 0 ? true : false;
-	}
+    @Override
+    public int compareTo(Student o) {
+        return studentId - o.getStudentId();
+    }
+
+    public boolean isLessThan(Student o) {
+        return studentId - o.getStudentId() < 0;
+    }
 }
--- src/com/mp1/disk/BlockReader.java
+++ src/com/mp1/disk/BlockReader.java
@@ -1,69 +1,65 @@
 package com.mp1.disk;
 
+import com.mp1.Buffer;
+import com.mp1.MemoryBuffer;
+import com.mp1.Student;
+
+import javax.naming.SizeLimitExceededException;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-
-import javax.naming.SizeLimitExceededException;
-
-import com.mp1.Buffer;
-import com.mp1.MemoryBuffer;
-import com.mp1.Student;
-
 /**
- * 
+ *
  */
 
 /**
  * @author saman
- *
  */
 
 public class BlockReader {
 
-	private BufferedReader br;
+    private BufferedReader br;
 
-	/**
-	 * @throws FileNotFoundException
-	 * 
-	 */
-	public BlockReader() {
-		br = null;
-	}
+    /**
+     * @throws FileNotFoundException
+     */
+    public BlockReader() {
+        br = null;
+    }
 
-	public void open(String file_name) throws FileNotFoundException {
-		br = new BufferedReader(new FileReader(file_name));
-	}
+    public void open(String file_name) throws FileNotFoundException {
+        br = new BufferedReader(new FileReader(file_name));
+    }
 
-	public void close() throws IOException {
-		if (br != null)
-			br.close();
-	}
+    public void close() throws IOException {
+        if (br != null)
+            br.close();
+    }
 
-	public void nextBlock(MemoryBuffer buffer) throws IOException {
-		String line;
-		int tuples = 0;
-		while (tuples < 40 && (line = br.readLine()) != null) {
-			++tuples;
-			Student student = new Student();
-			student.parseLine(line);
-			try {
-				buffer.add(student);
-			} catch (SizeLimitExceededException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	public void nextInputBuffer(Buffer inputBuffer) throws IOException {
-		String line;
-		for(int i = 0; i < inputBuffer.size && (line = br.readLine()) != null; i++) {
-			Student student = new Student();
-			student.parseLine(line);
-			inputBuffer.add(student);
-		}
-	}
+    public void nextBlock(MemoryBuffer buffer) throws IOException {
+        String line;
+        int tuples = 0;
+        while (tuples < 40 && (line = br.readLine()) != null) {
+            ++tuples;
+            Student student = new Student();
+            student.parseLine(line);
+            try {
+                buffer.add(student);
+            } catch (SizeLimitExceededException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void nextInputBuffer(Buffer inputBuffer) throws IOException {
+        String line;
+        for (int i = 0; i < inputBuffer.size && (line = br.readLine()) != null; i++) {
+            Student student = new Student();
+            student.parseLine(line);
+            inputBuffer.add(student);
+        }
+    }
 }
