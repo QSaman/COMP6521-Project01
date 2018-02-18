7a500a3fde1d3b50744e55ced0b29c2c893fb244
Saman Saadi <QSaman@users.noreply.github.com>
2018-02-18 18:07:09 -0500

    Add compare method to student. Implement equal method for student by considering all columns.

---
+++ src/com/mp1/DuplicateFinder.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.mp1;
+
+/**
+ * @author saman
+ *
+ */
+public class DuplicateFinder {
+
+	/**
+	 * 
+	 */
+	public DuplicateFinder() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
